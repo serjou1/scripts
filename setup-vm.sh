87a518913abc3035715ea9e7552fd07eb231aa53
Serhii <51600945+serjou1@users.noreply.github.com>
2025-03-02 23:11:25 +0200

    Create setup-vm.sh

---
+++ setup-vm.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install git curl -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+npm i -g pm2 pnpm
