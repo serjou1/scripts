fb503f2adfda29a4d9a07b14607a911bd086c7e4
Serhii <51600945+serjou1@users.noreply.github.com>
2025-11-05 03:16:35 +0200

    Update setup-vm.sh

--- setup-vm.sh
+++ setup-vm.sh
@@ -4,3 +4,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 22
 npm i -g pm2 pnpm
+pm2 install pm2-logrotate
+pm2 ls
+pm2 set pm2-logrotate:rotateInterval '0 0 * * *'
+pm2 set pm2-logrotate:dateFormat 'YYYY-MM-DD'
+pm2 set pm2-logrotate:retain 14
